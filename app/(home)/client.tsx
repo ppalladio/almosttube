753a228fae4608da7d9925b2a84467342238bdb9
ppalladio <nine.slide@gmail.com>
2025-03-13 18:38:58 +0100

    trpc setup

---
+++ app/(home)/client.tsx
@@ -0,0 +1,11 @@
+'use client'
+import { trpc } from "@/trpc/client";
+
+export default function HomePageClient() {
+	const [data] = trpc.hello.useSuspenseQuery({text:'prefetch'})
+
+	return <div>
+		client says {data.greeting}
+	</div>
+
+}
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,18 @@
-
+import { HydrateClient, trpc } from '@/trpc/server';
+import HomePageClient from './client';
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 
 export default function Home() {
-    return <div>CONTENT</div>;
+    void trpc.hello.prefetch({ text: 'prefetch' });
+
+    return (
+        <HydrateClient>
+            <Suspense fallback={<div>loading</div>}>
+                <ErrorBoundary fallback={<div>error</div>}>
+                <HomePageClient />
+				</ErrorBoundary>
+            </Suspense>
+        </HydrateClient>
+    );
 }
