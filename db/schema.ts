b644afe67082890eca84869b3fc8825ceb742308
ppalladio <nine.slide@gmail.com>
2025-03-13 07:24:51 +0100

    drizzle + neon setup

---
+++ db/schema.ts
@@ -0,0 +1,14 @@
+import { pgTable, text, timestamp, uniqueIndex, uuid } from 'drizzle-orm/pg-core';
+
+export const user = pgTable(
+    'user',
+    {
+        id: uuid('id').primaryKey().defaultRandom(),
+        clerkId: text('clerk_id').unique().notNull(),
+        name: text('name').notNull(),
+        imageUrl: text('image_url').notNull(),
+        createdAt: timestamp('created_at').notNull().defaultNow(),
+        updatedAt: timestamp('updated_at').notNull().defaultNow(),
+    },
+    (t) => [uniqueIndex('clerk_id_idx').on(t.clerkId)],
+);
