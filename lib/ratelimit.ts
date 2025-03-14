5c43b0a2b7ecc12aaed2e977245e8a5127995862
ppalladio <nine.slide@gmail.com>
2025-03-14 23:15:35 +0100

    rate limit with upstash

---
+++ lib/ratelimit.ts
@@ -0,0 +1,8 @@
+import { Ratelimit } from '@upstash/ratelimit';
+import { redis } from './redis';
+export const ratelimit = new Ratelimit({
+    redis,
+    // 10 reqs within 10s cause timeout
+    //https://upstash.com/docs/redis/sdks/ratelimit-ts/algorithms#sliding-window
+    limiter: Ratelimit.slidingWindow(10, '10s'),
+});
--- trpc/init.ts
+++ trpc/init.ts
@@ -1,10 +1,12 @@
 import { db } from '@/db';
 import { users } from '@/db/schema';
+import { ratelimit } from '@/lib/ratelimit';
 import { auth } from '@clerk/nextjs/server';
 import { initTRPC, TRPCError } from '@trpc/server';
 import { eq } from 'drizzle-orm';
 import { cache } from 'react';
 import SuperJSON from 'superjson';
+
 export const createTRPCContext = cache(async () => {
     const { userId } = await auth();
 
@@ -25,17 +27,26 @@ const t = initTRPC.context<Context>().create({
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+
 export const protectedProcedure = t.procedure.use(async function isAuthed(opts) {
     const { ctx } = opts;
-    console.log('cc', ctx);
     if (!ctx.ClerkUserId) {
         throw new TRPCError({ code: 'UNAUTHORIZED' });
     }
-	const [user] = await db.select().from(users).where(eq(users.clerkId,ctx.ClerkUserId)).limit(1)
-	console.log('user',user)
+    const [user] = await db.select().from(users).where(eq(users.clerkId, ctx.ClerkUserId)).limit(1);
+
+    if (!user) {
+        throw new TRPCError({ code: 'UNAUTHORIZED' });
+    }
+    const { success } = await ratelimit.limit(user.id);
+
+    if (!success) {
+        throw new TRPCError({ code: 'TOO_MANY_REQUESTS' });
+    }
     return opts.next({
         ctx: {
             ...ctx,
+            user,
         },
     });
 });
