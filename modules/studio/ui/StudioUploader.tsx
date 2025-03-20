9d7cfc7bac24034cdaace69bb62ff9a7ebe32331
ppalladio <nine.slide@gmail.com>
2025-03-20 07:13:07 +0100

    finish mux integration create responsive dialog create upload modal connect mux with db via webhook

--- modules/studio/ui/StudioUploader.tsx
+++ modules/studio/ui/StudioUploader.tsx
@@ -1,4 +1,6 @@
-import MuxUploader from '@mux/mux-uploader-react';
+import { Button } from '@/components/ui/button';
+import MuxUploader, { MuxUploaderDrop, MuxUploaderFileSelect, MuxUploaderProgress, MuxUploaderStatus } from '@mux/mux-uploader-react';
+import { UploadIcon } from 'lucide-react';
 interface StudioUploaderProps {
     endpoint?: string | null;
     onSuccess?: () => void;
@@ -6,9 +8,28 @@ interface StudioUploaderProps {
 const StudioUploader = ({ endpoint, onSuccess }: StudioUploaderProps) => {
     return (
         <div>
-            <MuxUploader endpoint={endpoint} />
+            <MuxUploader endpoint={endpoint} id="video-uploader" className="hidden group/uploder" />
+            <MuxUploaderDrop muxUploader="video-uploader" className="group/drop">
+                <div slot="heading" className="flex flex-col items-center gap-6">
+                    <div className="flex   items-center justify-center   rounded-full gap-6 bg-muted h-32 w-32">
+                        <UploadIcon className="size-10 text-muted-foreground group/drop-[&[active]]:animate-bounce transition-all duration-300" />
+                    </div>
+                    <div className="flex flex-col gap-2 text-center">
+                        <p className="text-sm">Drag and drop videos files to upload</p>
+                        <p className="text-xs text-muted-foreground">Your videos will be private until you publish them</p>
+                    </div>
+                    <MuxUploaderFileSelect muxUploader="video-uploader">
+                        <Button type={'button'} className="rounded-full">
+                            Select Files
+                        </Button>
+                    </MuxUploaderFileSelect>
+                </div>
+                <span slot="separator" className="hidden" />
+                <MuxUploaderStatus muxUploader="video-uploader" className="text-sm" />
+                <MuxUploaderProgress muxUploader="video-uploader" className="text-sm " type="percentage" />
+                <MuxUploaderProgress muxUploader="video-uploader" className="text-sm " type="bar" />
+            </MuxUploaderDrop>
         </div>
     );
 };
 export default StudioUploader;
- 
