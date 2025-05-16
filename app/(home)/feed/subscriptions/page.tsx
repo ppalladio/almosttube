fc73b486af0ae5029a3866316d061828145a0196
ppalladio <nine.slide@gmail.com>
2025-05-16 20:47:15 +0200

    home feed and subscription page

--- app/(home)/feed/subscriptions/page.tsx
+++ app/(home)/feed/subscriptions/page.tsx
@@ -1,5 +1,5 @@
 import { HydrateClient, trpc } from '@/trpc/server';
-import {SubscriptionView} from '@/modules/home/ui/view/SubscriptionsView';
+import { SubscriptionView } from '@/modules/home/ui/view/SubscriptionsView';
 export const dynamic = 'force-dynamic';
 
 export default async function Page() {
